607864a2e58bbd27fa95a8a535af8433ac1740f8
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-02-12 17:39:39 -0500

    Accept relay mail from subnet

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$1" = "postfix" ]; then
-  postconf queue_directory=/var/spool/postfix maillog_file=/dev/stdout
+  postconf queue_directory=/var/spool/postfix mydestination=localhost mynetworks_style=subnet
   if [ -n "$RELAYHOST" ]; then
     postconf "relayhost=$RELAYHOST"
   fi
